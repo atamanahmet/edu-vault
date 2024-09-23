cddef3047fcc90847a7451f84fbd9c2f3b901b21
atamanahmet <ahmtataman@proton.me>
2024-09-23 13:25:02 +0300

    modified:   index.js 	modified:   public/styles/styles.css 	new file:   views/create.ejs 	modified:   views/index.ejs 	modified:   views/partials/header.ejs

--- index.js
+++ index.js
@@ -3,12 +3,24 @@ import express from "express";
 const app = express();
 const port = 3000;
 app.use(express.static("public"));
-
+app.use(express.urlencoded({ extended: true }));
+var posted = {};
+var postTitle;
 
 app.get("/", (req, res) => {
-    res.render("index.ejs");
-})
+  res.render("index.ejs");
+});
 
-app.listen(port, (req,res) => {
-    console.log("Listenin on port : "+ port);
-})
+app.get("/create", (req, res) => {
+  res.render("create.ejs");
+});
+app.post("/create", (req, res) => {
+ res.locals.postTitle= req.body.postTitle;
+  res.locals.posted=posted;
+  res.render("index.ejs", posted);
+  console.log(posted);
+});
+
+app.listen(port, (req, res) => {
+  console.log("Listenin on port : " + port);
+});
