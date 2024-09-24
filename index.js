a4fa9701444d37ea75b0fbbc0e4e713803905caa
atamanahmet <ahmtataman@proton.me>
2024-09-24 16:00:30 +0300

    modified:   index.js 	modified:   public/styles/styles.css 	new file:   views/edit.ejs 	modified:   views/index.ejs 	modified:   views/partials/footer.ejs 	deleted:    views/viewPosts 	new file:   views/viewPosts.ejs

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ const app = express();
 const port = 3000;
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
-var posted={};
+var posted = { ataman: "asdasd"};
 var postTitle;
+var currentPost;
+var sendObj = {};
 
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  res.locals.posted = posted;
+  res.render("index.ejs", posted);
 });
 
 app.get("/create", (req, res) => {
@@ -16,14 +19,42 @@ app.get("/create", (req, res) => {
 });
 
 app.post("/create", (req, res) => {
- postTitle = req.body.postTitle;
-  res.locals.postTitle=postTitle;
-  console.log(postTitle);
+  postTitle = req.body.postTitle;
+  res.locals.postTitle = postTitle;
+
   posted[postTitle] = req.body.blogPost;
-  res.locals.posted=posted;
+  res.locals.posted = posted;
 
   res.render("index.ejs", posted);
 });
+
+app.post("/viewPosts", (req, res) => {
+
+  sendObj = {};
+
+  currentPost = Object.keys(req.body)[0];
+
+  sendObj[currentPost] = posted[currentPost];
+
+  res.locals.sendObj = sendObj;
+
+  res.render("viewPosts.ejs", sendObj);
+});
+
+app.post("/edit", (req,res) => {
+  sendObj={};
+  currentPost = Object.keys(req.body)[0];
+  sendObj[currentPost]= posted[currentPost];
+  res.locals.sendObj= sendObj;
+  
+  
+  res.render("edit.ejs", sendObj);
+})
+app.post("/delete", (req,res) => {
+  
+delete posted[Object.keys(req.body)[0]]
+res.redirect("/")
+})
 
 app.listen(port, (req, res) => {
   console.log("Listenin on port : " + port);
