8b2cef281de630edb3768b9c7d02f7028ce1a723
atamanahmet <ahmtataman@proton.me>
2024-10-04 11:28:07 +0300

    new file:   BlogAPI/api.json 	modified:   BlogAPI/index.js 	modified:   BlogAPI/package-lock.json 	modified:   BlogAPI/package.json 	modified:   BlogAPI/public/styles/main.css 	new file:   BlogAPI/public/styles/output.css 	new file:   BlogAPI/src/input.css 	new file:   BlogAPI/tailwind.config.js 	new file:   BlogAPI/views/edit.ejs 	modified:   BlogAPI/views/index.ejs 	modified:   BlogAPI/views/partials/footer.ejs 	modified:   BlogAPI/views/partials/header.ejs

---
+++ BlogAPI/index.js
@@ -0,0 +1,66 @@
+import express from "express";
+import axios from "axios";
+import fs from "fs";
+
+const app = express();
+const port = 3000;
+let posts = [
+    {
+      id: 1,
+      title: "The Rise of Decentralized Finance",
+      content:
+        "Decentralized Finance (DeFi) is an emerging and rapidly evolving field in the blockchain industry. It refers to the shift from traditional, centralized financial systems to peer-to-peer finance enabled by decentralized technologies built on Ethereum and other blockchains. With the promise of reduced dependency on the traditional banking sector, DeFi platforms offer a wide range of services, from lending and borrowing to insurance and trading.",
+      author: "Alex Thompson",
+      date: "2023-08-01T10:00:00Z",
+    },
+    {
+      id: 2,
+      title: "The Impact of Artificial Intelligence on Modern Businesses",
+      content:
+        "Artificial Intelligence (AI) is no longer a concept of the future. It's very much a part of our present, reshaping industries and enhancing the capabilities of existing systems. From automating routine tasks to offering intelligent insights, AI is proving to be a boon for businesses. With advancements in machine learning and deep learning, businesses can now address previously insurmountable problems and tap into new opportunities.",
+      author: "Mia Williams",
+      date: "2023-08-05T14:30:00Z",
+    },
+    {
+      id: 3,
+      title: "Sustainable Living: Tips for an Eco-Friendly Lifestyle",
+      content:
+        "Sustainability is more than just a buzzword; it's a way of life. As the effects of climate change become more pronounced, there's a growing realization about the need to live sustainably. From reducing waste and conserving energy to supporting eco-friendly products, there are numerous ways we can make our daily lives more environmentally friendly. This post will explore practical tips and habits that can make a significant difference.",
+      author: "Samuel Green",
+      date: "2023-08-10T09:15:00Z",
+    },
+  ];
+app.use(express.static("public"))
+//   fs.writeFileSync("api.json", JSON.stringify(posts));
+
+const api = JSON.parse(fs.readFileSync("api.json","utf-8"));
+// console.log(api);
+
+app.use(express.urlencoded({extended: true}));
+
+app.get("/", (req,res) => {
+    res.render("index.ejs", {content: api});
+})
+
+app.get("/edit/:id", (req,res) => {
+    const id = req.params.id;
+   api.forEach((item)=> {
+    if(item.id==id){
+        const content = item;
+        res.render("edit.ejs", {content: content})
+    }
+    else{
+        
+    }
+   })
+})
+// app.patch("/patch", (req,res) => {
+    
+// })
+
+app.listen(port, (req,res) => {
+    console.log("Server Online on port : "+port);
+})
+
+
+
