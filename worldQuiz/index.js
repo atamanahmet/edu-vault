652fe6eae377d49a1caa7415dead500d9caaf948
atamanahmet <ahmtataman@proton.me>
2024-10-05 17:26:09 +0300

    modified:   worldQuiz/index.js

--- worldQuiz/index.js
+++ worldQuiz/index.js
@@ -1,17 +1,59 @@
 import express from "express";
-import axios from "axios";
+// import axios from "axios";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
+let quiz=[];
+var randomCountry;
 
+const db= new pg.Client({
+    user: "postgres",
+    host: "localhost",
+    database: "world",
+    password: "aA!301331",
+    port: 5432
+})
+
+db.connect();
+
+db.query("select * from capitals", async (err,res) => {
+    try{
+        if (err) {
+            console.error("Error executing query", err.stack);
+        }
+        else {
+          quiz = await res.rows;
+        }
+    }
+    catch (err){
+        console.log(err.message);
+    }
+   
+})
+db.end();
+
+// async function randomGenerate(){
+//     randomCountry = await quiz[Math.floor((Math.random()*quiz.length)+1)]
+//     return randomCountry;
+// }
+// console.log(randomCountry);
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 let score = 0;
 
-app.get('/', (req, res) => {
-    const data=0;
-res.render("index.ejs", {content: data})
+app.get('/', async (req, res) => {
+    try{
+        quiz = await quiz[Math.floor((Math.random()*quiz.length)+1)]
+        
+        randomCountry["count"]=score;
+        res.render("index.ejs", {content: randomCountry})
+    }
+    catch (err){
+        console.log(err.message);
+    }
+    
     
 });
 app.listen(port, (req,res) => {
