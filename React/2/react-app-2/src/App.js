e7d62d6dc5b361b4a3bcd8579a7e078ea98b9c35
atamanahmet <ahmtataman@proton.me>
2024-10-20 13:20:32 +0300

    modified:   React/2/react-app-2/public/index.html 	modified:   React/2/react-app-2/src/App.js 	modified:   React/2/react-app-2/src/index.css 	modified:   React/2/react-app-2/src/index.js

--- React/2/react-app-2/src/App.js
+++ React/2/react-app-2/src/App.js
@@ -1,16 +1,7 @@
 import "./App.css";
 
 function App() {
-  return (
-    <div>
-      <h1>Test 123</h1>
-      <ul>
-        <li>Test 11 item</li>
-        <li>Test 11 item</li>
-        <li>Test 11 item</li>
-      </ul>
-    </div>
-  );
+  return <h1>asdasd</h1>;
 }
 
 export default App;
--- React/2/react-app-2/src/index.js
+++ React/2/react-app-2/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
