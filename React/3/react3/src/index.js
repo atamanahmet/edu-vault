0e207d040fb0e0b6b0665cfe0f10ccbcce245ec8
atamanahmet <ahmtataman@proton.me>
2024-10-20 14:20:59 +0300

    new file:   React/3/react3/.gitignore 	new file:   React/3/react3/README.md 	new file:   React/3/react3/package-lock.json 	new file:   React/3/react3/package.json 	new file:   React/3/react3/public/index.css 	new file:   React/3/react3/public/index.html 	new file:   React/3/react3/src/index.js

---
+++ React/3/react3/src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import "./index.css";
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+const name = "Aslı";
+
+root.render(
+  <div class="testDiv">
+    <p>Created by {name}</p>
+    <p>Copyright {new Date().getFullYear()}</p>
+  </div>
+);
