3fd79e01b60a13731655b840725b71ab7ece4c91
atamanahmet <ahmtataman@proton.me>
2024-10-20 13:39:20 +0300

    modified:   React/2/react-app-2/src/index.js

--- React/2/react-app-2/src/index.js
+++ React/2/react-app-2/src/index.js
@@ -5,13 +5,16 @@ import "./index.css";
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
 
+const asd = [1, 2, 3];
 const name = "Aslı";
-let randomNum = Math.floor(Math.random * 10);
+let randomNum = Math.floor(Math.random() * 10);
 
-console.log(randomNum);
 root.render(
   <div>
     <h1>Hello {name}</h1>
+    {asd.forEach((item) => {
+      <p>item</p>;
+    })}
     <p>Your lucky number is {randomNum}</p>
   </div>
 );
