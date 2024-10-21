b13b352bda64d6cbb9f44a878e50cc81616b2deb
atamanahmet <ahmtataman@proton.me>
2024-10-21 12:47:46 +0300

    new file:   React/3/time-based/src/App.jsx 	modified:   React/3/time-based/src/index.js

---
+++ React/3/time-based/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Heading from "./Heading.jsx";
+import Welcome from "./Welcome.jsx";
+import List from "./List";
+
+function App() {
+  return (
+    <div>
+      <Welcome></Welcome>
+      <Heading></Heading>
+      <List></List>
+    </div>
+  );
+}
+export default App;
--- React/3/time-based/src/index.js
+++ React/3/time-based/src/index.js
@@ -1,16 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import List from "./List.jsx";
-import Heading from "./Heading.jsx";
-import Welcome from "./Welcome.jsx";
+import App from "./App";
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
 
-root.render(
-  <div className="container">
-    <Welcome />
-    <Heading />
-    <List />
-  </div>
-);
+root.render(<App />);
