907a68acbae81b753af5467df5d062dbd0e35ce2
atamanahmet <ahmtataman@proton.me>
2024-10-21 11:20:57 +0300

    modified:   React/3/time-based/src/index.js

--- React/3/time-based/src/index.js
+++ React/3/time-based/src/index.js
@@ -3,11 +3,22 @@ import ReactDOM from "react-dom";
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
-let welcome;
-const getDate = new Date().getHours();
+let welcome = "Good ";
+const getDate = new Date().setHours();
+// const getDate = 1;
+
+if (getDate < 12) {
+  welcome = welcome + "morning vietnam";
+} else if (getDate >= 12 && getDate < 16) {
+  welcome = welcome + "afternoon vietnam";
+} else if (getDate >= 16 && getDate < 21) {
+  welcome = welcome + "evening vietnam";
+} else if (getDate >= 21 && getDate <= 24) {
+  welcome = welcome + "night vietnam";
+}
 
 root.render(
   <div className="container">
-    <h1>{getDate}</h1>
+    <h1>{welcome}</h1>
   </div>
 );
