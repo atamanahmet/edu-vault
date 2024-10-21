ce963b27ee15983927e6da3ebcf3bbe3ada57ff3
atamanahmet <ahmtataman@proton.me>
2024-10-21 12:35:46 +0300

    modified:   React/3/time-based/public/index.css 	modified:   React/3/time-based/src/index.js

--- React/3/time-based/src/index.js
+++ React/3/time-based/src/index.js
@@ -1,32 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import List from "./List.jsx";
+import Heading from "./Heading.jsx";
+import Welcome from "./Welcome.jsx";
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
-let welcome = "Good ";
-let setStyle = {
-  color: "black",
-};
-const getDate = new Date().getHours();
-
-if (getDate < 12) {
-  welcome = welcome + "morning vietnam";
-  setStyle.color = "red";
-} else if (getDate >= 12 && getDate < 16) {
-  welcome = welcome + "afternoon vietnam";
-  setStyle.color = "green";
-} else if (getDate >= 16 && getDate < 21) {
-  welcome = welcome + "evening vietnam";
-  setStyle.color = "orange";
-} else if (getDate >= 21 && getDate <= 24) {
-  welcome = welcome + "night vietnam";
-  setStyle.color = "navy";
-}
 
 root.render(
   <div className="container">
-    <h1 className="heading" style={setStyle}>
-      {welcome}
-    </h1>
+    <Welcome />
+    <Heading />
+    <List />
   </div>
 );
