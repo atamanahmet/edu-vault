198c96c985ae3912b16e9e8eec90a14435d0de82
atamanahmet <ahmtataman@proton.me>
2024-10-22 13:31:58 +0300

    modified:   React/3/time-based/src/App.jsx 	renamed:    React/3/time-based/src/components/contacts.jsx -> React/3/time-based/src/components/Card.jsx 	new file:   React/3/time-based/src/components/Details.jsx 	modified:   React/3/time-based/src/components/contactInfo.js

--- React/3/time-based/src/App.jsx
+++ React/3/time-based/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Heading from "./components/Heading.jsx";
 import Welcome from "./components/Welcome.jsx";
-import Contacts from "./components/contacts.jsx";
+import Card from "./components/Card.jsx";
 import Footer from "./components/footer.jsx";
 import contactInfo from "./components/contactInfo.js";
 
@@ -10,19 +10,19 @@ function App() {
     <div id="main">
       <Welcome></Welcome>
       <Heading></Heading>
-      <Contacts
+      <Card
         name={contactInfo[0].name}
         imgSrc={contactInfo[0].imgSrc}
         mail={contactInfo[0].mail}
         tel={contactInfo[0].tel}
       />
-      <Contacts
+      <Card
         name={contactInfo[1].name}
         imgSrc={contactInfo[1].imgSrc}
         mail={contactInfo[1].mail}
         tel={contactInfo[1].tel}
       />
-      <Contacts
+      <Card
         name={contactInfo[2].name}
         imgSrc={contactInfo[2].imgSrc}
         mail={contactInfo[2].mail}
--- React/3/time-based/src/components/Card.jsx
+++ React/3/time-based/src/components/Card.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import Avatar from "./Avatar.jsx";
+import Details from "./Details.jsx";
 
-function Contacts(props) {
+function Card(props) {
   return (
     <div className="contact">
       <h2 className="nameTitle">{props.name}</h2>
       <Avatar imgSrc={props.imgSrc} />
-      <p>{props.tel}</p>
-      <p>{props.mail}</p>
+      <Details info={props.tel}></Details>
+      <Details info={props.mail}></Details>
     </div>
   );
 }
-export default Contacts;
+export default Card;
