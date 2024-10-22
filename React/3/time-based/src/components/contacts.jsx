a306849d1b6372da43da2bce44c4c88fbac5c812
atamanahmet <ahmtataman@proton.me>
2024-10-22 12:39:10 +0300

    new file:   React/3/time-based/src/components/Avatar.jsx 	modified:   React/3/time-based/src/components/contacts.jsx

--- React/3/time-based/src/components/contacts.jsx
+++ React/3/time-based/src/components/contacts.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Avatar from "./Avatar.jsx";
 
 function Contacts(props) {
   return (
     <div className="contact">
       <h2 className="nameTitle">{props.name}</h2>
-      <img src={props.imgSrc} alt="profile" />
+      <Avatar imgSrc={props.imgSrc}></Avatar>
       <p>{props.tel}</p>
       <p>{props.mail}</p>
     </div>
