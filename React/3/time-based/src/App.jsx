10efbb1e3f2ca284a20f8e062b66fb872dc0e3c3
atamanahmet <ahmtataman@proton.me>
2024-10-22 12:23:45 +0300

    modified:   React/3/time-based/src/App.jsx 	new file:   React/3/time-based/src/components/contactInfo.js

--- React/3/time-based/src/App.jsx
+++ React/3/time-based/src/App.jsx
@@ -3,30 +3,33 @@ import Heading from "./components/Heading.jsx";
 import Welcome from "./components/Welcome.jsx";
 import Contacts from "./components/contacts.jsx";
 import Footer from "./components/footer.jsx";
+import contactInfo from "./components/contactInfo.js";
 
+console.log(contactInfo[0]);
 function App() {
   return (
     <div id="main">
       <Welcome></Welcome>
       <Heading></Heading>
       <Contacts
-        name="Ahmet"
-        imgSrc="https://placehold.co/200"
-        mail="ataman@gmail.co"
-        tel="+905886994747"
+        name={contactInfo[0].name}
+        imgSrc={contactInfo[0].imgSrc}
+        mail={contactInfo[0].mail}
+        tel={contactInfo[0].tel}
       />
       <Contacts
-        name="Aslı"
-        imgSrc="https://placehold.co/200"
-        mail="aslı@gmail.co"
-        tel="+905886994747"
+        name={contactInfo[1].name}
+        imgSrc={contactInfo[1].imgSrc}
+        mail={contactInfo[1].mail}
+        tel={contactInfo[1].tel}
       />
       <Contacts
-        name="Aslı"
-        imgSrc="https://placehold.co/200"
-        mail="aslı@gmail.co"
-        tel="+905886994747"
+        name={contactInfo[2].name}
+        imgSrc={contactInfo[2].imgSrc}
+        mail={contactInfo[2].mail}
+        tel={contactInfo[2].tel}
       />
+
       <hr />
       <Footer></Footer>
     </div>
---
+++ React/3/time-based/src/components/contactInfo.js
@@ -0,0 +1,21 @@
+const contactInfo = [
+  {
+    name: "Ahmet",
+    imgSrc: "https://placehold.co/200",
+    mail: "ahmet@gmail.co",
+    tel: "+905886994747",
+  },
+  {
+    name: "Aslı",
+    imgSrc: "https://placehold.co/200",
+    mail: "aslı@gmail.co",
+    tel: "+905886994747",
+  },
+  {
+    name: "Ataman",
+    imgSrc: "https://placehold.co/200",
+    mail: "ataman@gmail.co",
+    tel: "+905886994747",
+  },
+];
+export default contactInfo;
