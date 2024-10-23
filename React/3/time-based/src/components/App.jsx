4db11329b22a533a22b3f17fa9d0739b683c42e3
atamanahmet <ahmtataman@proton.me>
2024-10-23 12:00:32 +0300

    modified:   React/3/time-based/src/components/App.jsx

--- React/3/time-based/src/components/App.jsx
+++ React/3/time-based/src/components/App.jsx
@@ -2,21 +2,20 @@ import React from "react";
 import emojipedia from "../emojipedia.js";
 import Entry from "./Entry.jsx";
 
-function createEntry(emojiTerm) {
-  return (
-    <Entry
-      key={emojiTerm.id}
-      name={emojiTerm.name}
-      emoji={emojiTerm.emoji}
-      meaning={emojiTerm.meaning}
-    />
-  );
-}
-// console.log(emojipedia);
-// const newArray = emojipedia.map((item) => {
-//   return item.meaning.substring(0, 100);
-// });
-// console.log(newArray);
+// function createEntry(emojiTerm) {
+//   return (
+//     <Entry
+//       key={emojiTerm.id}
+//       name={emojiTerm.name}
+//       emoji={emojiTerm.emoji}
+//       meaning={emojiTerm.meaning}
+//     />
+//   );
+// }
+
+console.log(emojipedia);
+const newArray = emojipedia.map((item) => item.meaning.substring(0, 100));
+console.log(newArray);
 
 function App() {
   return (
@@ -25,7 +24,16 @@ function App() {
         <span>emojipedia</span>
       </h1>
 
-      <dl className="dictionary">{emojipedia.map(createEntry)}</dl>
+      <dl className="dictionary">
+        {emojipedia.map((term) => (
+          <Entry
+            key={term.id}
+            name={term.name}
+            emoji={term.emoji}
+            meaning={term.meaning}
+          />
+        ))}
+      </dl>
     </div>
   );
 }
