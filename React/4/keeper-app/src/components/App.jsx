0748187245f93d64a403df88cb4bac0838e3e9f3
atamanahmet <ahmtataman@proton.me>
2024-10-25 14:38:57 +0300

    modified:   React/4/keeper-app/src/components/App.jsx

--- React/4/keeper-app/src/components/App.jsx
+++ React/4/keeper-app/src/components/App.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
 import Note from "./Note.jsx";
+import notes from "../notes.js";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Note />
+      <div className="cardContainer">
+        {notes.map((note) => (
+          <Note key={note.key} title={note.title} content={note.content} />
+        ))}
+      </div>
       <Footer />
     </div>
   );
