009f00991b0dfe382593c9308c0134dcf0ea6b5d
atamanahmet <ahmtataman@proton.me>
2024-11-03 14:29:04 +0300

    modified:   React/5/public/index.css 	modified:   React/5/public/index.html 	modified:   React/5/src/components/App.jsx

--- React/5/src/components/App.jsx
+++ React/5/src/components/App.jsx
@@ -1,39 +1,53 @@
-import React from "react";
-import emojipedia from "../emojipedia.js";
-import Entry from "./Entry.jsx";
-
-// function createEntry(emojiTerm) {
-//   return (
-//     <Entry
-//       key={emojiTerm.id}
-//       name={emojiTerm.name}
-//       emoji={emojiTerm.emoji}
-//       meaning={emojiTerm.meaning}
-//     />
-//   );
-// }
-
-console.log(emojipedia);
-const newArray = emojipedia.map((item) => item.meaning.substring(0, 100));
-console.log(newArray);
+import React, { useState } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+// import Note from "./Note";
+// import CreateArea from "./CreateArea";
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [note, setNote] = useState("");
+  const [title, setTitle] = useState("");
+
+  function handleChange(event) {
+    const { value, name } = event.target;
+
+    if (name === "title") {
+      setTitle(value);
+    } else if (name === "note") {
+      setNote(value);
+    }
+    console.log(title);
+    console.log(note);
+  }
+  function addItem() {
+    setItems((prevState) => {
+      return [...prevState, { note: note, title: title }];
+    });
+    JSON.stringify(items);
+  }
+
   return (
     <div>
-      <h1>
-        <span>emojipedia</span>
-      </h1>
-
-      <dl className="dictionary">
-        {emojipedia.map((term) => (
-          <Entry
-            key={term.id}
-            name={term.name}
-            emoji={term.emoji}
-            meaning={term.meaning}
+      <Header />
+      {/* <CreateArea onDone={addItem} handleChange={handleChange} /> */}
+      <div>
+        <form>
+          <input name="title" placeholder="Title" onChange={handleChange} />
+          <textarea
+            name="note"
+            placeholder="Take a note..."
+            rows="3"
+            onChange={handleChange}
           />
-        ))}
-      </dl>
+          <button onClick={addItem}>Add</button>
+        </form>
+      </div>
+      {/* 
+      {items.map((item) => {
+        <Note title={item.title} />;
+      })} */}
+      <Footer />
     </div>
   );
 }
