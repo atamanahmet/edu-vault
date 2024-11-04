8638ef10463963b14aaa444cdd5f4d029977263d
atamanahmet <ahmtataman@proton.me>
2024-11-04 11:21:08 +0300

    modified:   React/5/src/components/App.jsx

--- React/5/src/components/App.jsx
+++ React/5/src/components/App.jsx
@@ -8,8 +8,6 @@ function App() {
   const [items, setItems] = useState([]);
 
   function addItem(note, title) {
-    // event.preventDefault();
-
     setItems((prevState) => {
       return [...prevState, { title: title, note: note }];
     });
