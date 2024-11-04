83d7be5d917af0f46aa86d8b8e53d95e72b57cc4
atamanahmet <ahmtataman@proton.me>
2024-11-04 11:25:26 +0300

    modified:   React/5/src/components/CreateArea.jsx

--- React/5/src/components/CreateArea.jsx
+++ React/5/src/components/CreateArea.jsx
@@ -1,23 +1,31 @@
 import React, { useState } from "react";
 
 function CreateArea(props) {
-  const [note, setNote] = useState("");
-  const [title, setTitle] = useState("");
+  // const [note, setNote] = useState("");
+  // const [title, setTitle] = useState("");
+  const [note, setNote] = useState({
+    title: "",
+    note: "",
+  });
 
   function handleChange(event) {
     const { value, name } = event.target;
 
-    if (name === "title") {
-      setTitle(value);
-    } else if (name === "note") {
-      setNote(value);
-    }
+    setNote((prev) => {
+      return { ...prev, [name]: value };
+    });
+
+    //   if (name === "title") {
+    //     setTitle(value);
+    //   } else if (name === "note") {
+    //     setNote(value);
+    //   }
   }
+
   function add(event) {
     event.preventDefault();
-    props.addItem(note, title);
-    setNote("");
-    setTitle("");
+    props.addItem(note.note, note.title);
+    setNote({ note: "", title: "" });
   }
 
   return (
@@ -27,14 +35,14 @@ function CreateArea(props) {
           name="title"
           placeholder="Title"
           onChange={handleChange}
-          value={title}
+          value={note.title}
         />
         <textarea
           name="note"
           placeholder="Take a note..."
           rows="3"
           onChange={handleChange}
-          value={note}
+          value={note.note}
         />
         <button onClick={add}>Add</button>
       </form>
