4453742bfb425a44634ecae4b1d7a56cda6c80d9
atamanahmet <ahmtataman@proton.me>
2024-11-04 11:20:32 +0300

    modified:   React/5/src/components/App.jsx

--- React/5/src/components/App.jsx
+++ React/5/src/components/App.jsx
@@ -6,46 +6,28 @@ import CreateArea from "./CreateArea";
 
 function App() {
   const [items, setItems] = useState([]);
-  const [note, setNote] = useState("");
-  const [title, setTitle] = useState("");
 
-  function handleChange(event) {
-    const { value, name } = event.target;
+  function addItem(note, title) {
+    // event.preventDefault();
 
-    if (name === "title") {
-      setTitle(value);
-    } else if (name === "note") {
-      setNote(value);
-    }
-  }
-
-  function addItem(event) {
-    event.preventDefault();
     setItems((prevState) => {
       return [...prevState, { title: title, note: note }];
     });
-    setTitle("");
-    setNote("");
   }
-  function deleteItem(event) {
-    const id = Number(event.target.id);
+
+  function deleteItem(id) {
     setItems(
       items.filter((item, index) => {
         return index !== id;
       })
     );
-    console.log(items);
   }
 
   return (
     <div>
       <Header />
-      <CreateArea
-        handleChange={handleChange}
-        addItem={addItem}
-        note={note}
-        title={title}
-      />
+      <CreateArea addItem={addItem} />
+
       {items.map((item, index) => {
         return (
           <Note
