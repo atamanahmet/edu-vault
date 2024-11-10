a9b3be2d215f1a0c67e3d501e4ed0b2182587ee3
atamanahmet <ahmtataman@proton.me>
2024-11-10 14:11:43 +0300

    new file:   React/7/tailwind/.gitignore 	new file:   React/7/tailwind/README.md 	new file:   React/7/tailwind/eslint.config.js 	new file:   React/7/tailwind/index.html 	new file:   React/7/tailwind/package-lock.json 	new file:   React/7/tailwind/package.json 	new file:   React/7/tailwind/postcss.config.js 	new file:   React/7/tailwind/src/App.jsx 	new file:   React/7/tailwind/src/assets/react.svg 	new file:   React/7/tailwind/src/index.css 	new file:   React/7/tailwind/src/main.jsx 	new file:   React/7/tailwind/tailwind.config.js 	new file:   React/7/tailwind/vite.config.js

---
+++ React/7/tailwind/src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1 className="text-4xl">Title</h1>
+      <p>paragraph</p>
+      <h2>Title 2</h2>
+      <ul>
+        <li>item1</li>
+        <li>item2</li>
+        <li>item3</li>
+      </ul>
+    </>
+  );
+}
+
+export default App;
