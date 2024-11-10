5e8074003b4a3721c692e43517d9c738e7db2418
atamanahmet <ahmtataman@proton.me>
2024-11-10 11:54:43 +0300

    deleted:    tailwind-react-edu/public/favicon.ico 	modified:   tailwind-react-edu/public/index.html 	deleted:    tailwind-react-edu/public/logo192.png 	deleted:    tailwind-react-edu/public/logo512.png 	deleted:    tailwind-react-edu/public/manifest.json 	deleted:    tailwind-react-edu/public/robots.txt 	deleted:    tailwind-react-edu/src/App.css 	deleted:    tailwind-react-edu/src/App.js 	new file:   tailwind-react-edu/src/App.jsx 	deleted:    tailwind-react-edu/src/App.test.js 	modified:   tailwind-react-edu/src/index.css 	modified:   tailwind-react-edu/src/index.js 	deleted:    tailwind-react-edu/src/logo.svg 	deleted:    tailwind-react-edu/src/reportWebVitals.js 	deleted:    tailwind-react-edu/src/setupTests.js

---
+++ Tailwind/tailwind-react-edu/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Title</h1>
+      <p>Paragraph asdfasasdasdasd</p>
+      <h2>Title 2</h2>
+    </div>
+  );
+}
+export default App;
