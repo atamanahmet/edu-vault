7519537752118675d4c46162a242efb29de3c4f4
atamanahmet <ahmtataman@proton.me>
2024-11-10 11:45:03 +0300

    modified:   tailwind-react/index.html 	modified:   tailwind-react/src/App.jsx 	modified:   tailwind-react/src/main.jsx

---
+++ Tailwind/tailwind-react/src/App.jsx
@@ -0,0 +1,12 @@
+// import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Title</h1>
+      <p>paragrapg asfasfaafafaffa</p>
+      <h2>Title 2</h2>
+    </div>
+  );
+}
+export default App;
