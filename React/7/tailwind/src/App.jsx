4d68ac40a814c20daa37cfdd513e62fe3564171a
atamanahmet <ahmtataman@proton.me>
2024-11-11 12:34:19 +0300

    modified:   React/7/tailwind/src/App.jsx 	new file:   React/7/tailwind/src/NavRight.jsx

--- React/7/tailwind/src/App.jsx
+++ React/7/tailwind/src/App.jsx
@@ -7,10 +7,15 @@ function App() {
   return (
     <>
       <div className="bg-primary h-14 ">
-        <div className="flex flex-row flex-nowrap items-center">
+        <div className="flex flex-row flex-nowrap gap-x-32">
           <Search />
           <Nav />
         </div>
+        {/* <div className="grid grid-cols-3 gap-x-7">
+          <p>1</p>
+          <p>2</p>
+          <p>3</p>
+        </div> */}
       </div>
     </>
   );
